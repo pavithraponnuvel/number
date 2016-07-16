4183b07171d4ccc92c200918e483ea642b861ed2
pavithraponnuvel <pavithraangel61@gmail.com>
2016-07-16 10:07:11 +0530

    Add files via upload

---
+++ power of numbers.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.*;
+public class powerofno {
+    public static void main(String[] args) {
+        int n1,n2;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no:");
+        n1=sc.nextInt();
+        System.out.println("enter power:");
+        n2=sc.nextInt();
+        System.out.println(Math.pow(n1,n2));
+
+    }
+    
+}
