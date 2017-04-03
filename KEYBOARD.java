80e27ae00a71ff23f148f1c96a6b3aba8f765176
pavithraponnuvel <pavithraangel61@gmail.com>
2017-04-03 15:23:36 +0530

    Add files via upload

---
+++ KEYBOARD.java
@@ -0,0 +1,58 @@
+
+package keyboard;
+
+import java.util.Scanner;
+
+public class Keyboard {
+
+   
+    public static void main(String[] args) {
+        
+        int[] a=new int[]{0,1,2,3,4,5,6,7,8,9};
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        switch(n){
+                case 0:{
+                System.out.println("nill");
+                break;
+            }   case 1:{
+                System.out.println("nill");
+                break;
+            }
+                case 2:{
+                System.out.println("ABC");
+                break;
+            }
+                case 3:{
+                System.out.println("DEF");
+                break;
+            }
+                case 4:{
+                System.out.println("GHI");
+                break;
+            }
+                case 5:{
+                System.out.println("JKL");
+                break;
+            }
+                case 6:{
+                System.out.println("MNO");
+                break;
+            }
+                case 7:{
+                System.out.println("PQRS");
+                break;
+            }
+                case 8:{
+                System.out.println("TUV");
+                break;
+            }
+                case 9:{
+                System.out.println("WXYZ");
+                break;
+            }
+                                              
+                
+        }
+    }
+}
